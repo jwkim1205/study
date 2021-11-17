005abe0e6d54580e10dc31ceb17d221457de1670
jwkim1205 <jwkim1205@github.com>
2021-11-17 21:49:17 +0900

    study: v-if 와 모달창 만들기, 숙제까지 완료

--- vues/vvuedonsan/src/App.vue
+++ vues/vvuedonsan/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="menu">
     <a v-for="navLink in nav" :key="navLink"> {{ navLink }} </a>
-  </div>
-
-  <img alt="Vue logo" src="./assets/logo.png">
-  
+  </div>  
   원룸샵
 
   <div>  
-    <h4 :class="클라스" :style="스타일">{{products[0]}} 원룸</h4>
+    <img src="./assets/img/room0.jpg" class="room-img">
+    <h4 :class="클라스" :style="스타일" @click="modal = true">{{products[0]}} 원룸</h4>
     <p>{{ price1 }} 만원</p>
     <button @click="increase0">허의매물신고</button>
     <span>신고수 : {{ 신고수[0] }}</span>
   </div>
 
   <div>
+    <img src="./assets/img/room1.jpg" class="room-img">
     <h4> {{ products[1] }}원룸</h4>
     <p>{{ price2 }} 만원</p>
     <button @click="increase1">허의매물신고</button>
@@ -22,10 +21,19 @@
   </div>
 
   <div>
+    <img src="./assets/img/room2.jpg" class="room-img">
     <h4> {{ products[2] }}원룸</h4>
     <p>{{ price3 }} 만원</p>
     <button @click="increase2">허의매물신고</button>
     <span>신고수 : {{ 신고수[2] }}</span>
+  </div>
+
+  <div class="black-bg" v-if="modal == true" @click="modal = false">
+    <div class="white-bg">
+      <button class="close" @click="modal = false">닫기</button>
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
   </div>
 
 </template>
@@ -41,7 +49,8 @@ export default {
         locate : ['역삼동원룸', '천호동원룸', '마포구원룸'],
         pay : [70, 100, 60]
       },
-      nav: ['Home', 'Products', 'Show', 'About']
+      nav: ['Home', 'Products', 'Show', 'About'],
+      modal: false,
     }
   },
   methods: {
@@ -78,5 +87,32 @@ export default {
 .menu a{
   color: white;
   padding: 10px;
+}
+
+.room-img{
+  width: 100%;
+  max-width: 450px;
+  margin-top: 40px;
+}
+
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+  top: 0px;
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
