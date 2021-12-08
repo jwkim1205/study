3982bd172c036fd4a6f1f71228f379a1ca0a8271
김진우 <jwkim@gimjin-uui-iMac.local>
2021-12-08 21:23:35 +0900

    study: github으로 배포해보기

---
+++ vues/vvuedonsan/src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav class="navigation">
+        <ul>
+            <li v-for="(item, i) in navItem" :key="item"><a :href=" '/' + navItem[i]">{{ navItem[i] }}</a></li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation',
+    props: {
+        navItem : Array
+    }
+}
+</script>
+
+<style>
+    .navigation{
+        background-color: #0088cc;
+        border-radius: 5px;
+    }
+    .navigation ul{
+        display: flex;
+        padding: 20px;
+        justify-content: center;
+        list-style: none;
+    }
+    .navigation ul li{
+        margin: 0px 5px;
+    }
+    .navigation ul li a{
+        color: #ffffff;
+        font-weight: bold;
+        text-decoration: none;
+    }
+
+</style>
